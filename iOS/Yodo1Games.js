caaec06236ec93a6eeac117a50401b5750c174ba
Yodo1 MAS <85629293+yodo1mas@users.noreply.github.com>
2022-09-13 10:27:29 +0100

    Update

--- iOS/Yodo1Games.js
+++ iOS/Yodo1Games.js
@@ -1,90 +1,83 @@
-import { NativeModules, NativeEventEmitter, Alert } from 'react-native'
-const { Yodo1MASAds } = NativeModules
+import {Alert, NativeEventEmitter, NativeModules} from 'react-native'
 
-export const hasReward = () => {
-  // Your code here to check if the user has earned a reward
-  // From Redux, AsyncStorage
-  return true
-}
-
-export const setReward = () => {
-  // Your code here to confirm that the user earned a reward
-  // Redux, AsyncStorage, etc...
-}
-
-export const isAdsInitialized = () => {
-  // Your code here to check if the ads are initialized from other components
-}
-
-export const setAdsInitialized = (isInitialized) => {
-  // Your code here to set the adsInitialized state
-  // Redux, AsyncStorage, etc...
-}
-
-export const showAdExplainer = () =>
-  Alert.alert('You must watch the entire Ad to earn the reward!')
-
-  export const ShowRewardGivenAlert = () =>
-  Alert.alert('Reward Added')
-  
-const handleYodoEvent = ({ value }) => {
-  __DEV__ && console.log(`MAS Event: ${value}`)
-  switch (value) {
-    // Event received when Ads initialization is successful
-    case 'onMasInitSuccessful':
-      Yodo1MASAds.showBannerAds() // This has effect only in Android
-     // setAdsInitialized(true)
-     // isAdsInitialized(true);
-      
-      break
-
-    case 'onMasInitFailed':
-      setTimeout(() => Yodo1MASAds.initMasSdk(), 5000) // Try again in 5 seconds
-      
-      break
-
-    // User earned a reward!
-    case 'reward-onAdvertRewardEarned':
-    setTimeout(() => hasReward() ? null : ShowRewardGivenAlert(), 5000)
-      setReward()
-      break
-
-    // User closed the Ad, let's check if he earned a reward
-    case 'reward-onAdClosed':
-      setTimeout(() => hasReward() ? null : showAdExplainer(), 500)
-      break
-
-    // Something went wrong, let's skip the checks on reward
-    case 'reward-onAdError':
-      setReward()
-      break
-  }
-}
-
+const {Yodo1MASAds} = NativeModules
+export const hasReward =
+    () => {
+      // Your code here to check if the user has earned a reward
+      // From Redux, AsyncStorage
+      return true
+    } export const setReward =
+        () => {
+          // Your code here to confirm that the user earned a reward
+          // Redux, AsyncStorage, etc...
+        } export const isAdsInitialized =
+            () => {
+              // Your code here to check if the ads are initialized from other
+              // components
+            } export const setAdsInitialized =
+                (isInitialized) => {
+                  // Your code here to set the adsInitialized state
+                  // Redux, AsyncStorage, etc...
+                } export const showAdExplainer = () =>
+                    Alert.alert(
+                        'You must watch the entire Ad to earn the reward!')
+export const ShowRewardGivenAlert = () => Alert.alert('Reward Added')
+const handleYodoEvent =
+    ({value}) => {
+      __DEV__ && console.log(`MAS Event: ${value}`)
+      switch (value) {
+        // Event received when Ads initialization is successful
+        case 'onMasInitSuccessful':
+          Yodo1MASAds.showBannerAds()  // This has effect only on Android
+          // setAdsInitialized(true)
+          // isAdsInitialized(true);
+          break case 'onMasInitFailed': setTimeout(
+              () => Yodo1MASAds.initMasSdk(), 5000)  // Try again in 5 seconds
+          break
+              // User earned a reward!
+              case 'reward-onRewardAdEarned': setTimeout(
+                  () => hasReward() ? null : ShowRewardGivenAlert(), 5000)
+          setReward()
+          break
+              // Reward Ad loaded, you can check by this method if the ad is
+              // loaded
+              case 'reward-onRewardAdLoaded':
+                  setTimeout(() => hasReward() ? null : showAdExplainer(), 500)
+          break
+              // Reward Ad Failed to Open
+              case 'reward-onRewardAdOpened':
+                  setTimeout(() => hasReward() ? null : showAdExplainer(), 500)
+          break
+              // User closed the Ad, let's check if he earned a reward
+              case 'reward-onRewardAdClosed':
+                  setTimeout(() => hasReward() ? null : showAdExplainer(), 500)
+          break
+              // Something went wrong, let's skip the checks on reward
+              case 'reward-onRewardAdFailedToLoad':
+                  // setReward()
+              break
+      }
+    }
 // Call me on App Initialization
 export const registerYodoAds = () => {
   const eventEmitter = new NativeEventEmitter(Yodo1MASAds)
   const eventListener = eventEmitter.addListener('adEvent', handleYodoEvent)
   Yodo1MASAds.initMasSdk()
   return eventListener
-}
-
-export const showBannerAds = async () => {
-  //const adsAvailable = await Yodo1MASAds.isInitialized()
-  //adsAvailable && 
+} export const showBannerAds = async () => {
+  // const adsAvailable = await Yodo1MASAds.isInitialized()
+  // adsAvailable &&
   Yodo1MASAds.showBannerAds()
-  
-}
-
-export const showInterstitialAds = async () => {
-  //const adsAvailable = await Yodo1MASAds.isInitialized()
-  //adsAvailable && 
+} export const dismissBannerAds = async () => {
+  // const adsAvailable = await Yodo1MASAds.isInitialized()
+  // adsAvailable &&
+  Yodo1MASAds.hideBannerAds()
+} export const showInterstitialAds = async () => {
+  // const adsAvailable = await Yodo1MASAds.isInitialized()
+  // adsAvailable &&
   Yodo1MASAds.showIntertstialAds()
-  
-}
-
-export const showRewardedAds = async () => {
- // const adsAvailable = await Yodo1MASAds.isInitialized()
- // adsAvailable ? 
- Yodo1MASAds.showRewardedAds() //: setReward()
+} export const showRewardedAds = async () => {
+  // const adsAvailable = await Yodo1MASAds.isInitialized()
+  // adsAvailable ?
+  Yodo1MASAds.showRewardedAds()  //: setReward()
 }
--- iOS/Yodo1MASAds.m
+++ iOS/Yodo1MASAds.m
@@ -2,111 +2,160 @@
 //  Yodo1MASAds.m
 //  Created by Yodo1 on 02/25/22.
 //
- 
 #import <Foundation/Foundation.h>
-#import <Yodo1MasCore/Yodo1Mas.h>
-#import <React/RCTEventEmitter.h>
 #import <React/RCTBridgeModule.h>
- 
-@interface Yodo1MASAds: RCTEventEmitter <RCTBridgeModule, Yodo1MasRewardAdDelegate, Yodo1MasInterstitialAdDelegate, Yodo1MasBannerAdDelegate>
+#import <React/RCTEventEmitter.h>
+#import <Yodo1MasCore/Yodo1Mas.h>
+#import "Yodo1MasBannerAdView.h"
+@interface Yodo1MASAds : RCTEventEmitter <RCTBridgeModule,
+                                          Yodo1MasRewardDelegate,
+                                          Yodo1MasInterstitialDelegate,
+                                          Yodo1MasBannerAdDelegate>
 @end
- 
-@implementation Yodo1MASAds
-{
+@implementation Yodo1MASAds {
   bool hasListeners;
 }
- 
--(void)startObserving {
+
+- (void)startObserving {
   hasListeners = YES;
 }
- 
--(void)stopObserving {
+- (void)stopObserving {
   hasListeners = NO;
 }
- 
 - (NSArray<NSString *> *)supportedEvents {
-  return @[@"adEvent"];
+  return @[ @"adEvent" ];
 }
- 
-- (void) sendEvent:(NSString *) event {
+- (void)sendEvent:(NSString *)event {
   if (hasListeners) {
-    [self sendEventWithName:@"adEvent" body:@{@"value": event}];
-    NSLog(@"Yodo1MASAds: sent Event to RN: %@", event);
+    [self sendEventWithName:@"adEvent" body:@{@"value" : event}];
+    // NSLog(@"Yodo1MASAds: sent Event to RN: %@", event);
   }
 }
- 
+
+#pragma mark - Yodo1MasInterstitialDelegate
+- (void)onInterstitialAdLoaded:(Yodo1MasInterstitialAd *)ad {
+  [self sendEvent:@"interstitial-onInterstitialAdLoaded"];
+}
+
+- (void)onInterstitialAdFailedToLoad:(Yodo1MasInterstitialAd *)ad
+                           withError:(Yodo1MasError *)error {
+  [self sendEvent:@"interstitial-onInterstitialAdFailedToLoad"];
+}
+
+- (void)onInterstitialAdOpened:(Yodo1MasInterstitialAd *)ad {
+  [self sendEvent:@"interstitial-onInterstitialAdOpened"];
+}
+
+- (void)onInterstitialAdFailedToOpen:(Yodo1MasInterstitialAd *)ad
+                           withError:(Yodo1MasError *)error {
+  [self sendEvent:@"interstitial-onInterstitialAdFailedToOpen"];
+  [ad loadAd];
+}
+
+- (void)onInterstitialAdClosed:(Yodo1MasInterstitialAd *)ad {
+  [self sendEvent:@"interstitial-onInterstitialAdClosed"];
+  [ad loadAd];
+}
+
+#pragma mark - Yodo1MasRewardDelegate
+- (void)onRewardAdLoaded:(Yodo1MasRewardAd *)ad {
+  [self sendEvent:@"reward-onRewardAdLoaded"];
+}
+
+- (void)onRewardAdFailedToLoad:(Yodo1MasRewardAd *)ad
+                     withError:(Yodo1MasError *)error {
+  [self sendEvent:@"reward-onRewardAdFailedToLoad"];
+}
+
+- (void)onRewardAdOpened:(Yodo1MasRewardAd *)ad {
+  [self sendEvent:@"reward-onRewardAdOpened"];
+}
+
+- (void)onRewardAdFailedToOpen:(Yodo1MasRewardAd *)ad
+                     withError:(Yodo1MasError *)error {
+  [self sendEvent:@"reward-onRewardAdFailedToOpen"];
+  [ad loadAd];
+}
+
+- (void)onRewardAdClosed:(Yodo1MasRewardAd *)ad {
+  [self sendEvent:@"reward-onRewardAdClosed"];
+  [ad loadAd];
+}
+
+- (void)onRewardAdEarned:(Yodo1MasRewardAd *)ad {
+  [self sendEvent:@"reward-onRewardAdEarned"];
+}
+#pragma mark - Yodo1MasAdDelegate
 - (void)onAdOpened:(Yodo1MasAdEvent *)event {
-  [self sendEvent:@"reward-onAdOpened"];
+  [self sendEvent:@"banner-onAdOpened"];
 }
- 
+
 - (void)onAdClosed:(Yodo1MasAdEvent *)event {
-  [self sendEvent:@"reward-onAdClosed"];
+  [self sendEvent:@"banner-onAdClosed"];
 }
- 
-- (void)onAdError:(Yodo1MasAdEvent *)event error:(Yodo1MasError *)error {
-  switch (event.type) {
-    case Yodo1MasAdTypeReward: {
-      [self sendEvent:@"reward-onAdError"];
-      break;
-    }
-    case Yodo1MasAdTypeInterstitial: {
-      [self sendEvent:@"interstitial-onAdError"];
-      break;
-    }
-    case Yodo1MasAdTypeBanner: {
-      [self sendEvent:@"banner-onAdError"];
-      break;
-    }
-  }
+
+- (void)onAdvertError:(Yodo1MasAdEvent *)event error:(Yodo1MasError *)error {
+  [self sendEvent:@"banner-onAdvertError"];
 }
- 
-- (void)onAdRewardEarned:(Yodo1MasAdEvent *)event {
-  [self sendEvent:@"reward-onAdvertRewardEarned"];
-}
- 
-/*RCT_EXPORT_METHOD(isInitialized:(RCTPromiseResolveBlock) resolve rejecter:(RCTPromiseRejectBlock) reject ) {
-  NSLog(@"Yodo1MASAds: isInitialized: %@", [Yodo1Mas sharedInstance]. ? @"YES" : @"NO");
-  resolve(@([Yodo1Mas sharedInstance].initialize));
+/*RCT_EXPORT_METHOD(isInitialized:(RCTPromiseResolveBlock) resolve
+rejecter:(RCTPromiseRejectBlock) reject ) { NSLog(@"Yodo1MASAds: isInitialized:
+%@", [Yodo1Mas sharedInstance]. ? @"YES" : @"NO"); resolve(@([Yodo1Mas
+sharedInstance].initialize));
 }*/
- 
 RCT_EXPORT_METHOD(initMasSdk) {
   dispatch_async(dispatch_get_main_queue(), ^{
-    [Yodo1Mas sharedInstance].rewardAdDelegate = self;
-    [Yodo1Mas sharedInstance].interstitialAdDelegate = self;
-    
     Yodo1MasAdBuildConfig *config = [Yodo1MasAdBuildConfig instance];
-    config.enableAdaptiveBanner = YES;
-    config.enableUserPrivacyDialog = YES;
+    config.enableUserPrivacyDialog = YES;  // default value is NO
     [[Yodo1Mas sharedInstance] setAdBuildConfig:config];
- 
-    [[Yodo1Mas sharedInstance] initWithAppKey:@"r6r4u8L7dM" successful:^{
-      [self sendEvent:@"onMasInitSuccessful"];
-    } fail:^(NSError * _Nonnull error) {
-      [self sendEvent:@"onMasInitFailed"];
-    }];
+    [[Yodo1Mas sharedInstance] initWithAppKey:@"r6r4u8L7dM"
+        successful:^{
+          [self sendEvent:@"onMasInitSuccessful"];
+        }
+        fail:^(NSError *_Nonnull error) {
+          [self sendEvent:@"onMasInitFailed"];
+        }];
+    // Banner Ad delegate
+    [Yodo1Mas sharedInstance].bannerAdDelegate = self;
+
+    // IntertsitialAds Delegate and Load
+    [Yodo1MasInterstitialAd sharedInstance].adDelegate = self;
+    [[Yodo1MasInterstitialAd sharedInstance] loadAd];
+
+    // Reward Ad loading
+    [Yodo1MasRewardAd sharedInstance].adDelegate = self;
+    [[Yodo1MasRewardAd sharedInstance] loadAd];
   });
 }
- 
 RCT_EXPORT_METHOD(showRewardedAds) {
   dispatch_async(dispatch_get_main_queue(), ^{
-    [[Yodo1Mas sharedInstance] showRewardAd];
+    [[Yodo1MasRewardAd sharedInstance]
+        showAdWithPlacement:@"Your Placement Id"];
   });
 }
- 
 RCT_EXPORT_METHOD(showIntertstialAds) {
   dispatch_async(dispatch_get_main_queue(), ^{
-    [[Yodo1Mas sharedInstance] showInterstitialAd];
+    [[Yodo1MasInterstitialAd sharedInstance]
+        showAdWithPlacement:@"Your Placement"];
   });
 }
- 
 RCT_EXPORT_METHOD(showBannerAds) {
- dispatch_async(dispatch_get_main_queue(), ^{
-    Yodo1MasAdBannerAlign align = Yodo1MasAdBannerAlignBottom | Yodo1MasAdBannerAlignHorizontalCenter;
+  dispatch_async(dispatch_get_main_queue(), ^{
+    Yodo1MasAdBannerAlign align =
+        Yodo1MasAdBannerAlignBottom | Yodo1MasAdBannerAlignHorizontalCenter;
     CGPoint point = CGPointMake(10.0f, 10.0f);
     [[Yodo1Mas sharedInstance] showBannerAdWithAlign:align offset:point];
   });
 }
+RCT_EXPORT_METHOD(hideBannerAds) {
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [[Yodo1Mas sharedInstance] dismissBannerAd];
 
+    BOOL destroy = NO;  // if destroy == YES, the ads displayed in the next call
+                        // of showBanner are different. if destroy == NO, the
+                        // ads displayed in the next call of showBanner are the same
+    [[Yodo1Mas sharedInstance] dismissBannerAdWithDestroy:destroy];
+  });
+}
 // To export a module named Yodo1MASAds
 RCT_EXPORT_MODULE(Yodo1MASAds);
 @end
